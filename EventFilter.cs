023495f91bfc7635878cc42d7c5bf96c159b6323
Pain Brioché <Kakoloukiam@gmail.com>
2024-02-21 15:13:02 +0100

    nump version

--- EventFilter.cs
+++ EventFilter.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 namespace COM3D2.EventFilter
 {
-    [BepInPlugin("COM3D2.EventFilter", "Event Filter", "1.0.0")]
+    [BepInPlugin("COM3D2.EventFilter", "Event Filter", "1.0.1")]
     public partial class EventFilter : BaseUnityPlugin
     {
         //Main Instance
